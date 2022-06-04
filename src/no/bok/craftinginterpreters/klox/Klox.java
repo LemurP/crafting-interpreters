13a8bad93c9806e9c8cf2a8f679f4f8aa4c1de5f
Peter Norstein <peter.norstein@gmail.com>
2022-06-04 22:14:12 +0200

    Create barebones start for interpreter
    This should be the bare minimum to begin implementing the first chapter
    of Crafting Interpreters book.

---
+++ src/no/bok/craftinginterpreters/klox/Klox.java
@@ -0,0 +1,60 @@
+package no.bok.craftinginterpreters.klox;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Klox {
+
+  // Exit codes are used as indicated in https://www.freebsd.org/cgi/man.cgi?query=sysexits&manpath=FreeBSD+4.3-RELEASE
+  public static final int EX_USAGE = 64; // The command was used incorrectly
+  public static final int EX_DATAERR = 65; // The (user's) input data was incorrect in some way
+  static boolean hadError = false;
+
+  public static void main(String[] args) throws IOException {
+    if (args.length > 1) {
+      System.out.println("Usage: klox [script]");
+      System.exit(EX_USAGE);
+    } else if (args.length == 1) {
+      runFile(args[0]);
+    } else {
+      runPrompt();
+    }
+
+  }
+
+  private static void runFile(String path) throws IOException {
+    byte[] bytes = Files.readAllBytes(Paths.get(path));
+    run(new String(bytes, Charset.defaultCharset()));
+
+    //Scan, Parse, Interpret here
+
+    if (hadError) {
+      System.exit(EX_DATAERR);
+    }
+  }
+
+  private static void runPrompt() throws IOException {
+    InputStreamReader input = new InputStreamReader(System.in);
+    BufferedReader reader = new BufferedReader(input);
+
+    for (; ; ) { // [repl]
+      System.out.print("> ");
+      String line = reader.readLine();
+      if (line == null) {
+        break;
+      }
+      run(line);
+      hadError = false;
+    }
+  }
+
+  private static void run(String source) {
+    System.out.println("This interpreter is currently not functional. Input was: " + source);
+    //Scanner scanner = new Scanner(source);
+    //List<Token> tokens = scanner.scanTokens();
+  }
+}
